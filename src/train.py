2b98c4bc5722e60da3bed3a2af4bad3629425f0f
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2018-10-14 19:49:28 -0300

    Modelo aparantemente funcioanndo e capaz de aprender

--- src/train.py
+++ src/train.py
@@ -3,19 +3,25 @@ import os
 DIRNAME = os.path.dirname(__file__)
 sys.path.append(os.path.join(DIRNAME, "models/keras-deeplab-v3-plus-master/"))
 from model import Deeplabv3
-from utils import load_dataset
+from utils import load_dataset, IMAGE_SIZE
+from keras.callbacks import TensorBoard
 
 X_train, Y_train, X_dev, Y_dev, X_test, Y_test = load_dataset()
 
 deeplab_model = Deeplabv3(input_shape=(200, 200, 3), classes=2)
+
 for layer in deeplab_model.layers[:147]:
     layer.trainable = False
-for index, layer in enumerate(deeplab_model.layers):
-    print(index, layer, layer.trainable)
+# for index, layer in enumerate(deeplab_model.layers):
+#     print(index, layer, layer.trainable)
 
 deeplab_model.compile(loss='binary_crossentropy',
-                      optimizer='adam',
-                      metrics=['accuracy'])
+              optimizer='adam',
+              metrics=['accuracy'])
+
+tensorboard = TensorBoard(batch_size=16, update_freq="batch")
 
 deeplab_model.fit(X_train, Y_train, batch_size=16, verbose=2,
-                  validation_data=(X_dev, Y_dev))
+          validation_data=(X_dev, Y_dev),
+          epochs=10,
+          callbacks=[tensorboard])
