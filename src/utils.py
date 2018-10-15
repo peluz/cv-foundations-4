7b22ed4d3695387aa9f6f0e098b9053283155fa3
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2018-10-15 00:57:06 -0300

    adiciona algumas utils

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
 import os
+import matplotlib.pyplot as plt
+import random
 
 DIRNAME = os.path.dirname(__file__)
 TRAIN_IMAGE_PATH = os.path.join(DIRNAME, "../data/images/vienna16.tif")
@@ -39,4 +41,33 @@ def sample_image(image, image_size):
     samples = [image[row:row+image_size, col:col+image_size, :] for row in range(0, 5000 - image_size + 1, image_size) for col in range(0, 5000 - image_size + 1, image_size)]
     return np.stack(samples, axis=0)
 
-load_dataset()
+def show_dataset():
+    X_train, Y_train, X_dev, Y_dev, X_test, Y_test = load_dataset()
+    images = [random.randint(0, 5000*5000/IMAGE_SIZE/IMAGE_SIZE - 1) for _ in range(20)]
+
+    show_grid(X_train[images], Y_train[images])
+    show_grid(X_dev[images], Y_dev[images])
+    show_grid(X_test[images], Y_test[images])
+
+
+def show_grid(train, label):
+    fig = plt.figure(figsize=(100, 100))
+    columns = 4
+    rows = 5
+    for i in range(0, rows):
+        fig.add_subplot(rows, columns, 4 * i + 1)
+        plt.imshow(train[2*i])
+        fig.add_subplot(rows, columns, 4 * i + 2)
+        plt.imshow(label[2*i].squeeze() * 255, cmap="gray")
+        fig.add_subplot(rows, columns, 4 * i + 3)
+        plt.imshow(train[2*i + 1])
+        fig.add_subplot(rows, columns, 4 * i + 4)
+        plt.imshow(label[2*i + 1].squeeze() * 255, cmap="gray")
+    plt.show()
+
+
+def baseline():
+    X_train, Y_train, X_dev, Y_dev, X_test, Y_test = load_dataset()
+    print(Y_train.sum()/5000/5000)
+    print(Y_dev.sum()/5000/5000)
+    print(Y_test.sum()/5000/5000)
