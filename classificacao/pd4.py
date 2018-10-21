64f26edd2ca225cf43ef7954996ac390ed41dc29
Pedro Henrique Luz de Araujo <pedrohluzaraujo@gmail.com>
2018-10-21 19:43:52 -0300

    Revert "Classification finished, I guess."
    This reverts commit b1c4243d0e6378012a218bb7c016a5b23e40c717.

--- classificacao/pd4.py
+++ classificacao/pd4.py
@@ -6,63 +6,51 @@ from feature_selection import feature_relation
 from features_extraction import *
 from glcm import *
 from f_measure import f_measure
-from bayes import *
-from extract_interest import *
-from sklearn import svm
 
-def features_array(img_predio, img_nao_predio):
-    matrix_glcm_p = glcm_mounter(img_predio)
-    matrix_glcm_p /= np.sum(matrix_glcm_p)
-    matrix_glcm_np = glcm_mounter(img_nao_predio)
-    matrix_glcm_np /= np.sum(matrix_glcm_np)
-
-    features_array = []
-    features_array.append(all_features(matrix_glcm_p))
-    features_array.append(all_features(matrix_glcm_np))
-    features_array = np.array(features_array, dtype = np.float32)
-    print(features_array)
-    return features_array
 
 #########################################################
 # PROCESSAMENTO DAS GLCMS E SELEÇÃO DAS CARACTERÍSTICAS #
 #########################################################
-def main():
-    img = cv.imread("data/vienna16.tif", -1)
-    img_aux = cv.imread("data/gt/vienna16.tif", 0)
-    # img_predio, img_nao_predio = extract(img, img_aux)
-    # cv.imwrite("data/img_predio.tif", img_predio)
-    # cv.imwrite("data/img_n_predio.tif", img_nao_predio)
-    img_predio = cv.imread("data/img_predio_train.tif", 0)
-    img_nao_predio = cv.imread("data/img_n_predio_train.tif", 0)
-    train_features_array = features_array(img_predio, img_nao_predio)
 
-    # Descubro qual feature devo ignorar #
-    # feature_relation(train_features_array)
+img = cv.imread("data/vienna16.tif", -1)
+height, width, channels = img.shape
+print(height, width)
+train_features_array = []
+print("\nProcessing...\n")
+# Para cada imagem eu a transformo em cinza, adquiro sua matriz GLCM e suas respectivas features,
+# então as guardo em um Array 
+image = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-    img = cv.imread("data/vienna5.tif", -1)
-    img_aux = cv.imread("data/gt/vienna5.tif", 0)
-    img_predio = cv.imread("data/img_predio_teste.tif", 0)
-    img_nao_predio = cv.imread("data/img_n_predio_teste.tif", 0)
-    test_features_array = features_array(img_predio, img_nao_predio)
-    
-    img = cv.imread("data/vienna22.tif", -1)
-    img_aux = cv.imread("data/gt/vienna22.tif", 0)
-    img_predio = cv.imread("data/img_predio_final.tif", 0)
-    img_nao_predio = cv.imread("data/img_n_predio_final.tif", 0)
-    final_features_array = features_array(img_predio, img_nao_predio)
-    
-    #############################
-    # CLASSIFICAÇÃO DAS IMAGENS #
-    ##############################
-    clf = svm.SVR(kernel='poly', C=1e3, degree=2)
-    # clf = svm.SVC(kernel = 'poly', degree= 2)
-    clf.fit(train_features_array, [0,1])
-    print(clf.score(test_features_array, [0, 1]))
-    print(clf.predict(test_features_array))
-    clf.fit(test_features_array, [0,1])
-    print(clf.predict(final_features_array))
-    print(clf.score(test_features_array, [0, 1]))
+matrix_glcm = glcm_mounter(image)
+matrix_glcm /= np.sum(matrix_glcm)
+print(matrix_glcm)
+print(np.shape(matrix_glcm))
 
-if __name__ == '__main__':
-    main()
+features = all_features(matrix_glcm)
+train_features_array.append(features)
+
+train_features_array = np.array(train_features_array, dtype = np.float32)
+print(train_features_array)
+print(np.shape(train_features_array))
+
+# Descubro qual feature devo ignorar #
+feature_relation(train_features_array)
+
+image = cv.imread("data/vienna5.tif", -1)
+image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
+test_features_array = []
+matrix_glcm = glcm_mounter(image)
+matrix_glcm /= np.sum(matrix_glcm)
+
+features = all_features(matrix_glcm, 3) # Não calculo a feature eliminada #
+test_features_array.append(features)
+
+test_features_array = np.array(test_features_array, dtype = np.float32)
+print(test_features_array)
+print(np.shape(test_features_array))
+
+######################################################
+# CÁLCULO DAS DISTÂNCIAS E CLASSIFICAÇÃO DAS IMAGENS #
+######################################################
+
 
